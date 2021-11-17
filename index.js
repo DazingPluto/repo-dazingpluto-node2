9cb29a1a1fbd1ade85b5e43ac0511755eecdf4dd
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2021-11-16 18:39:52 -0600

    :)

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ console.log(myModule);
 
 myModule.helloWorld();
 console.log(myModule.helloWorld());
+
+const faker = require ('faker');
+let randomeName= faker.name.findName();
+console.log(randomeName);
+
+const radomAdress = {
+    streetAddress: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zipCode: faker.address.zipCode()
+}
+console.log(radomAdress);
+
+const axious = require ('axios');
+
+axious.get('https://api.github.com/users/romebell')
+.then(function(response) {
+    console.log(response.data);
+})
+.catch(function(err) {
+    console.log(err);
+});
